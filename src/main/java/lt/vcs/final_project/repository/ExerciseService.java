7b08fd67f023b759513030d3660652b4182f7614
Zilvinas <zilvinas.pusnys@gmail.com>
2023-06-22 21:46:55 +0300

    Added Exercise service

---
+++ src/main/java/lt/vcs/final_project/repository/ExerciseService.java
@@ -0,0 +1,30 @@
+package lt.vcs.final_project.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ExerciseService {
+    @Autowired
+    private ExercisePoolRepository exercisePoolRepository;
+
+    public List<Exercise> getAllExercises(){return (List<Exercise>) exercisePoolRepository.findAll();}
+
+    public Exercise getExerciseById(int id){
+        return exercisePoolRepository.findById(id).get();
+    }
+
+    public List<Exercise> getExerciseByName(String name){
+        return exercisePoolRepository.getExerciseLike("%" + name + "%");
+    }
+
+    public void saveExercise(Exercise exercise){
+        exercisePoolRepository.save(exercise);
+    }
+
+    public void deleteExercise(int id){
+        exercisePoolRepository.deleteById(id);
+    }
+}
