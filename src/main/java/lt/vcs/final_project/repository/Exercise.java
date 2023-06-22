0eab2973e80861f3248713a711e5b6677e540940
Zilvinas <zilvinas.pusnys@gmail.com>
2023-06-22 21:46:26 +0300

    Added Exercise class

---
+++ src/main/java/lt/vcs/final_project/repository/Exercise.java
@@ -0,0 +1,35 @@
+package lt.vcs.final_project.repository;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "exercise_pool")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exercise_id")
+    private Integer id;
+
+    @Column(name = "exercise_name")
+    private String exerciseName;
+
+    @Column(name = "exercise_description")
+    private String exerciseDescription;
+
+    @Column(name = "is_equipment_needed")
+    private Boolean isEquipmentNeeded;
+
+    public Exercise(String exerciseName, String exerciseDescription, Boolean isEquipmentNeeded) {
+        this.exerciseName = exerciseName;
+        this.exerciseDescription = exerciseDescription;
+        this.isEquipmentNeeded = isEquipmentNeeded;
+    }
+}
