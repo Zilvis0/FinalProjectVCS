84480326338967273cd41afc47ebfe64565306dd
Zilvinas <zilvinas.pusnys@gmail.com>
2023-06-22 15:18:52 +0300

    Initial commit

---
+++ src/test/java/lt/vcs/final_project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vcs.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
