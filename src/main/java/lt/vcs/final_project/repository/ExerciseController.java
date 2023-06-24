1c35f77cfa3d17968a45921b51caa22a6e16dfa6
Zilvinas <zilvinas.pusnys@gmail.com>
2023-06-24 11:52:49 +0300

    Created html templates
    Created template pages for homepage and for displaying all exercises from exercise_pool table in database

--- src/main/java/lt/vcs/final_project/repository/ExerciseController.java
+++ src/main/java/lt/vcs/final_project/repository/ExerciseController.java
@@ -2,16 +2,25 @@ package lt.vcs.final_project.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping(path = "/SportSite")
+@RequestMapping(path = "/exercises")
 public class ExerciseController {
+
     @Autowired
     private ExerciseService exerciseService;
 
-    // http://localhost:8080/SportSite/home
-    @GetMapping(path = "/home")
-    public String displayHome(){return "home";}
+//    http://localhost:8080/exercises/all
+    @GetMapping(path = "/all")
+    public String fetchAllExercises(Model model){
+        List<Exercise> list = exerciseService.getAllExercises();
+        model.addAttribute("exercise_list", list);
+        return "/exercise_pool/exercise_pool";
+    }
+
 }
---
+++ src/main/resources/sql_scripts/sql_exercise_pool_data.sql
@@ -0,0 +1,38 @@
+-- CREATE SCHEMA exercise_pool;
+
+-- SELECT * FROM final_project.exercise_pool;
+
+-- INSERT INTO final_project.exercise_pool
+-- (exercise_id, exercise_name, exercise_description, is_equipment_needed)
+-- VALUE(1,
+-- "pull_ups",
+--  "The pull-up is a closed-chain movement where the body is suspended by the hands, gripping a bar or other implement at a distance typically wider than shoulder-width, and pulled up. As this happens, the elbows flex and the shoulders adduct and extend to bring the elbows to the torso",
+--   true);
+
+-- INSERT INTO final_project.exercise_pool
+-- (exercise_id, exercise_name, exercise_description, is_equipment_needed)
+-- VALUE(2,
+-- "burpee",
+--  "Start by standing erect with the arms by the side, feet should-width apart. Bend the knees, squatting down to place the hands on the floor in front of the feet. Putting the bodyweight on the hands, the legs are thrust back to a push-up position with a straight line from the shoulders to the heels. Next pull the legs back and return to the squatting position. Jump back to the starting standing position.",
+--   false);
+
+-- INSERT INTO final_project.exercise_pool
+-- (exercise_id, exercise_name, exercise_description, is_equipment_needed)
+-- VALUE(3,
+--  "biceps curl",
+--  "A full repetition consists of bending or 'curling' the elbow until it is fully flexed, then slowly lowering the weight to the starting position. The torso should remain upright instead of swinging back and forth, as doing so transfers the load away from the biceps and onto other muscles, reducing the effectiveness of the exercise.",
+--   true);
+
+-- INSERT INTO final_project.exercise_pool
+-- (exercise_id, exercise_name, exercise_description, is_equipment_needed)
+-- VALUE(4,
+--  "bench press",
+--   "The person performing the exercise lies on their back on a flat bench with a barbell grasped in both hands. They lower the barbell to chest level until it touches the chest, then press the barbell upwards, extending the arms until the elbows are locked out. This is one repetition (rep). ",
+--  true);
+
+-- INSERT INTO final_project.exercise_pool
+-- (exercise_id, exercise_name, exercise_description, is_equipment_needed)
+-- VALUE(5,
+--  "squat",
+--  "the trainee lowers their hips from a standing position and then stands back up.",
+--   false);
