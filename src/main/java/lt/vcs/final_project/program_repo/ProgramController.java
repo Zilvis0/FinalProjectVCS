740d075c3b099f02868aaad2cbdfff88603c60da
Zilvinas <zilvinas.pusnys@gmail.com>
2023-06-28 18:02:13 +0300

    created mapping for programs by id and creating new ones

--- src/main/java/lt/vcs/final_project/program_repo/ProgramController.java
+++ src/main/java/lt/vcs/final_project/program_repo/ProgramController.java
@@ -3,8 +3,7 @@ package lt.vcs.final_project.program_repo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +19,30 @@ public class ProgramController {
         List<Program> list = programService.getAllPrograms();
         model.addAttribute("program_list", list);
         return "/programs/programs";
+    }
+
+    @GetMapping(path = "/id/{id}")
+    public String fetchProgramById(Model model, @PathVariable int id){
+        Program program = programService.getProgramById(id);
+        model.addAttribute("program_object", program);
+        return "/programs/programDetails";
+    }
+
+    //    http://localhost:8080/programs/new
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String createProgram(Model model)
+            {
+        model.addAttribute("key_program", new Program());
+        return "/programs/newProgram";
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public String createProgram(
+            Model model,
+            @ModelAttribute(value = "key_program") Program program
+    ){
+        programService.saveProgram(program);
+        model.addAttribute("program_object", program);
+        return "/programs/programDetails";
     }
 }
